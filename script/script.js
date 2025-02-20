ee9eec575b2a5e9f0787423b601d530cd04a5178
Ami Halsey <cro19030@byui.edu>
2025-02-19 17:13:42 -0700

    added json

---
+++ script/script.js
@@ -0,0 +1,24 @@
+fetch("./script/projects.json").then((res) => res.json()).then((data) => {
+    console.log(data);
+    renderProjects(data.projects)
+});
+
+function renderProjects(projects) {
+    const projectHtml = projects.map(projectTemplate).join("");
+    console.log(projectHtml)
+    const projectsElement = document.getElementById('projects');
+    projectsElement.innerHTML = projectHtml;
+}
+
+
+function projectTemplate(project) {
+    return `
+    <div class="project-container">
+        <h3>${project.title}</h3>
+        <a href="${project.url}">
+            <img src="${project.image}" alt="${project.title}">
+        </a>
+        <p>${project.description}</p>
+    </div>
+    `
+}
